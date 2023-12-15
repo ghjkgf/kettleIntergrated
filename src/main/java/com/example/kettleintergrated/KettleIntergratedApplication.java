0b4574dc8ea2ff3c3b83fda390d3459982ff4125
zsl <zhaosenlin@dbhs.com.cn>
2023-12-15 10:41:38 +0800

    Initial commit

---
+++ src/main/java/com/example/kettleintergrated/KettleIntergratedApplication.java
@@ -0,0 +1,13 @@
+package com.example.kettleintergrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KettleIntergratedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KettleIntergratedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kettleintergrated/KettleIntergratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kettleintergrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KettleIntergratedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
